05af3b13317d43e3686eedbe2f72d79f611c2ad4
Kevin Burg <kburg@foursquare.com>
2015-02-06 20:51:07 -0500

    It's a webserver now

--- src/surrogate/Surrogate.java
+++ src/surrogate/Surrogate.java
@@ -1,39 +1,25 @@
 package surrogate;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Vector;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
 public class Surrogate {
 	
-	static void babble(Model model, Integer len) {
-		Vector<String> words = new Vector<String>();
-		words.add("to"); words.add("any");
-		for (int i = 2; i < len; i++) {
-			String p2 = words.get(i-2);
-			String p1 = words.get(i-1);
-			String next = model.predict(p2, p1);
-			words.add(next);
-		}
-		
-		BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
-		try {
-			for (int i = 0; i < words.size(); i++) {
-				wr.write(words.get(i) + " ");
-			} 
-			wr.write("\n");
-			wr.flush();
-		}	
-		catch (IOException e) {
-			e.printStackTrace();
+	private static ServerSocket serverSocket;
+
+	
+
+	public static void main(String[] args) throws IOException {
+		serverSocket = new ServerSocket(8080); // Start, listen on port 8080
+		while (true) {
+			try {
+				Socket s = serverSocket.accept(); 
+				new ClientHandler(s); 
+			} catch (Exception x) {
+				System.out.println(x);
+			}
 		}
 	}
-	
-	public static void main(String[] args) {
-		String filename = args[0];
-		Model model = new Model(filename);
-		babble(model, 25);
-	}
-	
+
 }
